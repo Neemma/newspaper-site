c0d8a5b34b1a333f11632a93b1963fff68c8800b
bethlydia <bmutuba@kabarak.ac.ke>
2023-12-10 02:35:04 +0300

    mm

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+
+
 const HomePage = () => {
+
   const pagenumber = 1;
   return (
 
     <div className="container">
       <div className="header">
-        <h1>Welcome to My Newspaper Website</h1>
+        {/* <h3 className='imagedecor'>woah did you actually have to
+          find out what this says?
+          That's really mortiying, </h3> */}
+
+        <h2 className='h2'> WEDNESDAY NOT-TIMES</h2>
+        {/* 
+        <h3 className='imagedecor'>
+          I mean, why are you ruining this for me dude.
+          These are just pretty buildings in the distance man.
+          Gosh. Font:WaveFro</h3> */}
+
+        <h2 className='h2right'> A STANDARD NEWSPAPER</h2>
+
+
+
       </div>
 
       {/* <nav>
@@ -28,22 +46,19 @@ const HomePage = () => {
             </section>
           </div>
 
-          <div className="item long">
-            <section>
-              <h2>Image</h2>
-              <article>
-                <h3>Article Title 2</h3>
-                <p>Article content...</p>
-              </article>
+          <div className="item long Image">
+            <section className='Image'>
+              <h3>The Climber</h3>
+              <Image src="/climber.svg" alt="Climber" width={400} height={500} />
             </section>
           </div>
 
           <div className="item small">
             <section>
-              <h2>News Section 3</h2>
               <article>
-                <h3>Article Title 3</h3>
-                <p>Article content...</p>
+                <p>Article content..."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                </p>
               </article>
             </section>
           </div>
@@ -140,10 +155,10 @@ const HomePage = () => {
         </div>
       </main>
       <div className="footer">
-      
+
       </div>
-      <div className='pagenumber'> 
-         {pagenumber}
+      <div className='pagenumber'>
+        {pagenumber}
       </div>
 
     </div>
--- app/pageone/page.js
+++ app/pageone/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./style.css";
+import Image from 'next/image';
 
 const PageOne = () => {
   const pagenumber = 2;
@@ -7,7 +8,7 @@ const PageOne = () => {
     <div className="container">
       <div className="headercontainer">
         <div className="header1">
-          <h1>IMAGE</h1>
+          <Image src="/urahara.svg" alt="Header Image" width={700} height={250} />
         </div>
 
         <div className="header2container">
@@ -29,17 +30,15 @@ const PageOne = () => {
       </div>
 
       <div className="heading">
-        <p>TITLE HEADING</p>
+        <p className='heading'>TITLE HEADING</p>
       </div>
+
       <main>
         <div className="grid">
           <div className="item small">
             <section>
-              <h2>News Section 1</h2>
-              <article>
-                <h3>Article Title 1</h3>
-                <p>Article content...</p>
-              </article>
+              <h3>MISSING:</h3>
+            <Image src="/oldman.svg" alt="Header Image" width={300} height={300} />
             </section>
           </div>
 
@@ -84,6 +83,7 @@ const PageOne = () => {
                   <p>Article content...</p>
                 </article>
               </section>
+              
               <div className='item3'>
                 <section>
                   <article>
@@ -104,33 +104,20 @@ const PageOne = () => {
                 <h3>Article Title 1</h3>
                 <p>Article content...</p>
               </article>
-              <div className="caption2">
-              <p>rrrrrrrrrrddddddd</p>
-            </div>
+            
             </section>
            
             <div className='item longest'>
-            <section>
-            <article>
-              sdf
-              </article>
-            </section>
+            <section>bhb</section>
             </div>
-          </div>
-
-        
-
+          </div> 
         </div>
-        
-
         </main>
       <div className="footer">
-  
       </div>
       <div className='pagenumber'> 
          {pagenumber}
       </div>
-
     </div>
   );
 };
