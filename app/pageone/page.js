f9a618fa83acdbf33a09bc53fe18c9434264d324
bmutuba <bmutuba@safaricom.co.ke>
2024-04-22 09:48:31 +0300

    ok2

--- app/pageone/page.js
+++ app/pageone/page.js
@@ -256,7 +256,7 @@ const PageOne = () => {
               <section>
                 <h2>EQUALITY: THE FEVER DREAM</h2>
                 <article>
-                  <h3>A Monkey's Evolution</h3>
+                  <h3>A Monkey&apos;s Evolution</h3>
                   <p>Articspiciat Sed utSed d ut perspiciati
                     Sed utSed ut perspici consequunturatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat volupta perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
                     Sdantiuia nsequunturntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam qua Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.erat voluptatem.ntent...</p>
@@ -287,7 +287,7 @@ const PageOne = () => {
                     <p>LOCAL TRUCK DRIVER WANTED<br></br>
                       60+ HOURS/WK  <br></br>
                       CALL: 0722772277
-                      DRIVER'S LICENSE REQUIRED
+                      DRIVER&apos;S LICENSE REQUIRED
                       <br></br><br></br>         
                       WAITER/WAITRESS WANTED<br></br>
                       KFC KANGEMI <br></br>
