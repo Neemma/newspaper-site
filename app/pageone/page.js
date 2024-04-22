2678f2f38b12d8776248d6615cf8fb20a4b8d931
bmutuba <bmutuba@safaricom.co.ke>
2024-04-22 14:58:01 +0300

    ok

--- app/pageone/page.js
+++ app/pageone/page.js
@@ -212,12 +212,12 @@ const PageOne = () => {
                 </section>
               </div>
               <div className="item smallll f">
-                {/* <Image src="/trace.svg" alt="Header Image" width={200} height={90} /> */}
                 <div className="image-container">
                   <motion.div
                     variants={funkyHoverAnimation}
                     initial="initial"
                     whileHover="hover"
+                    className="imageoh"
                     style={{ width: 200, height: 90, position: 'relative' }}
                   >
                     <Image src="/trace.svg" alt="Header Image" layout="fill" objectFit="contain" />
@@ -231,7 +231,7 @@ const PageOne = () => {
                 {modalVisible && (
                   <div className="modal">
                     <div className='glasseffect'>
-                      <Coupon />
+                      <Coupon /> 
                       <span className="close" onClick={handleCloseModal}>&times;</span>
                     </div>
                   </div>
@@ -239,7 +239,7 @@ const PageOne = () => {
               </div>
             </div>
 
-            <div className="item small a">
+            <div className="item small x a">
               <section>
                 <h2>FREEDOM</h2>
                 <article>
@@ -252,7 +252,7 @@ const PageOne = () => {
               </section>
             </div>
 
-            <div className="item small b">
+            <div className="item small x b">
               <section>
                 <h2>EQUALITY: THE FEVER DREAM</h2>
                 <article>
@@ -266,61 +266,97 @@ const PageOne = () => {
 
             </div>
 
+            <div className='columns'>
 
-            <div className="small split">
-              <div className='item1'>
-                <section>
-                  <h2>BECOMING A BETTER PERSON</h2>
-                  <article>
-                    <h3>When are dreams too big?</h3>
-                    <p>Ar Sed ut perspiciati iste qui s unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                      Sed ut iunde omnis o quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam qua                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.erat voluptatem.
-                      ticle</p>
-                  </article>
-                </section>
-              </div>
+              <div className="small split">
+                <div className='item1'>
+                  <section>
+                    <h2>BECOMING A BETTER PERSON</h2>
+                    <article>
+                      <h3>When are dreams too big?</h3>
+                      <p>Ar Sed ut perspicia d ut iunde omnis o quisquam est, ti iste qui s unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                        Sed ut iunde omnis o quisquad ut iunde omnis o quisquam est, d ut iunde omnis o quisquam est, m est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam qua                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.erat voluptatem.
+                        ticle</p>
+                    </article>
+                  </section>
+                </div>
 
-              <div className='item2'>
-                <section>
-                  <article>
-                    <h2>VACANCIES</h2>
-                    <p>LOCAL TRUCK DRIVER WANTED<br></br>
-                      60+ HOURS/WK  <br></br>
-                      CALL: 0722772277
-                      DRIVER&apos;S LICENSE REQUIRED
-                      <br></br><br></br>         
-                      WAITER/WAITRESS WANTED<br></br>
-                      KFC KANGEMI <br></br>
-                      CALL: 0711111222
-                      <br></br><br></br>
-                      SALES REP WANTED<br></br>
-                      10,000+ COMMISSION <br></br>
-                      CALL: 0788888822
-                     
-                    </p>
-                  </article>
-                </section>
-
-                <div className='item3'>
+                <div className='item2'>
                   <section>
                     <article>
-                      <p> Wishing Jonathan Kinyanjui a Happy 76th Birthday! 
-                      <br></br>From Your Cousins- John and James.
-                        <br></br><br></br>
-                        NEED SOME QUICK CASH? <br></br>
+                      <h2>VACANCIES</h2>
+                      <p>LOCAL TRUCK DRIVER WANTED<br></br>
+                        60+ HOURS/WK  <br></br>
                         CALL: 0722772277
-                        FOR NEXT TO ZERO INTEREST LOANS!
+                        DRIVER&apos;S LICENSE REQUIRED
                         <br></br><br></br>
-                        HOUSES FOR RENT <br></br>
-                        BEDSITTER: 15,000 <br></br>
-                        ONE BEDROOM: 25,000 <br></br>
+                        WAITER/WAITRESS WANTED<br></br>
+                        KFC KANGEMI <br></br>
                         CALL: 0711111222
-                        KANGEMI, EQUITY
-                        {/* WATER AND ELECTRICITY INCLUDED */}
+                        <br></br><br></br>
+                        SALES REP WANTED<br></br>
+                        10,000+ COMMISSION <br></br>
+                        CALL: 0788888822
 
                       </p>
                     </article>
                   </section>
+
+                  <div className='item3'>
+                    <section>
+                      <article>
+                        <p> Wishing Jonathan Kinyanjui a Happy 76th Birthday!
+                          <br></br>From Your Cousins- John and James.
+                          <br></br><br></br>
+                          NEED SOME QUICK CASH? <br></br>
+                          CALL: 0722772277
+                          FOR NEXT TO ZERO INTEREST LOANS!
+                          <br></br><br></br>
+                          HOUSES FOR RENT <br></br>
+                          BEDSITTER: 15,000 <br></br>
+                          ONE BEDROOM: 25,000 <br></br>
+                          CALL: 0711111222
+                          KANGEMI, EQUITY
+                          {/* WATER AND ELECTRICITY INCLUDED */}
+
+                        </p>
+                      </article>
+                    </section>
+                  </div>
+                </div>
+
+              </div>
+
+
+
+              <div className='flex'>
+                <div className="item medium d">
+                  <section>
+                    <article>
+                      <h3>And The World Fell Into Ruin </h3>
+                      <p>Ar Sed iciatit voluptatem accusantium doloremque lau
+                        erspiciat iciatit voluptatem accusantium doloremque lau
+                        erspiciciatit voluptatem accusantium doloremque lau
+                        erspiciat iciatit voluptatem accusantium doloremque lau
+                        erspiciat iciatit voluptatem accusantium doloremque lau
+                        erspiciat iciatit voluptatem accusantium doloremque lau
+                        erspiciat iciatit voluptatem accusantium doloremque lau
+                        erspiciat iat ut persiciatit voluptatem accusantium doloremque lau
+                        erspiciat piciatis unde omnis iste natus Sed ut perspiciatis unde omnis isteSed ut perspiciatis unde omnis iste natuSed ut perspiciatis unde omnis iste natu natuSed ut perspiciatis unde omnis iste natuerror sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non nused quia non numquam eius t labore et dolore magnam aliquam quaerat voluptatem.
+                        atis unde omnis iste natuspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam alised quia non numquam eius sed quia non numquam eius quam quaerat
+                      </p>
+                    </article>
+
+                  </section>
+                </div>
+
+                <div className='item longest e'>
+                  <section> <p>Ar Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                    Ssed quia non non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                    Sed ut perspiciataut fugit, sed de omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                    voluptatem accusantium doloa non non numquam eius remque lauue ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                  </p></section>
+
                 </div>
               </div>
 
@@ -328,37 +364,7 @@ const PageOne = () => {
 
 
 
-            <div className="item medium d">
-              <section>
-                <article>
-                  <h3>And The World Fell Into Ruin </h3>
-                  <p>Ar Sed iciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat iat ut persiciatit voluptatem accusantium doloremque lau
-                    erspiciat piciatis unde omnis iste natus Sed ut perspiciatis unde omnis isteSed ut perspiciatis unde omnis iste natuSed ut perspiciatis unde omnis iste natu natuSed ut perspiciatis unde omnis iste natuerror sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non nused quia non numquam eius t labore et dolore magnam aliquam quaerat voluptatem.
-                    Sed ut perSediciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat iciatit voluptatem accusantium doloremque lau
-                    erspiciat  ut perspiciatis unde omnis iste natuSed ut perspiciatis unde omnis iste natuspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam alised quia non numquam eius sed quia non numquam eius quam quaerat
-                  </p>
-                </article>
 
-              </section>
-
-              <div className='item longest e'>
-                <section> <p>Ar Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Ssed quia non numquam eius sed quia non numquam eius sed quia non numquam eius sed quia non numquam eius sed quia non numquam eius sed quia non numquam eius ed ut Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Sed ut perspiciataut fugit, sed de omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                  Sed ut Sed ut perspiciatis unde omniSed ut perspiciatis unde omnis iste natus iste natus error sit voluptatem accusantium doloremque lauue ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                </p></section>
-
-              </div>
-            </div>
           </div>
         </main>
 
